b93f5d813012a31dff694377eb27336f239dbbf3
Khanh <khanh@codelynx.io>
2020-07-14 00:17:34 +0700

    add docs folder

---
+++ docs/precache-manifest.80b471d345ebf795ad5490a45150ec55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2e33c804ac9366d068098a76954b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce0462a3d5c7bcec5659",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "4ad873d7efa055f16112",
+    "url": "/static/css/main.cb0ef300.chunk.css"
+  },
+  {
+    "revision": "ce0462a3d5c7bcec5659",
+    "url": "/static/js/2.ca6ef588.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.ca6ef588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad873d7efa055f16112",
+    "url": "/static/js/main.e6c866ea.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
