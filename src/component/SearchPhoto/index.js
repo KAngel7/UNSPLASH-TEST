e2481fd4b14d3ab792df89dc9020e2c1b8e63c9b
Khanh <khanh@codelynx.io>
2020-07-14 00:10:44 +0700

    feat store query params

--- src/component/SearchPhoto/index.js
+++ src/component/SearchPhoto/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pagination, Input, Row, Col } from 'antd';
+import { Pagination, Input, Row, Col, Spin } from 'antd';
 import { debounce } from 'lodash';
 import './style.css';
 import { search } from '../../service/unsplashApi';
@@ -7,19 +7,25 @@ import { search } from '../../service/unsplashApi';
 class SearchPhoto extends React.Component {
   constructor() {
     super();
+    const urlParams = new URLSearchParams(window.location.search);
     this.state = {
-      loading: false,
-      searchString: "",
-      page: 0,
+      loading: !!urlParams.get('searchString'),
+      searchString: urlParams.get('searchString') || "",
+      page: urlParams.get('page') || 1,
       data: {
         results: []
       }
     };
   }
+  componentWillMount() {
+    if (this.state.searchString) {
+      this.searchPhoto();
+    }
+  }
   searchPhoto = () => {
     this.setState({ loading: true });
+    window.history.pushState(null, null, `?searchString=${encodeURIComponent(this.state.searchString)}&page=${this.state.page}`);
     search(this.state.searchString, this.state.page).then(result => {
-      console.log(result.data);
       this.setState({ data: result.data });
     }).finally(() => {
       this.setState({ loading: false });
@@ -28,23 +34,30 @@ class SearchPhoto extends React.Component {
   searchPhotoDebounce = debounce(this.searchPhoto, 200);
   handleChangeSearchString = e => {
     this.setState({ searchString: e.currentTarget.value, page: 1 }, this.searchPhotoDebounce);
-  }
+  };
   handleChangePage = page => {
     this.setState({ page }, this.searchPhoto);
-  }
+  };
+  listPhoto = () => (
+    <Row style={{ margin: -10 }}>
+      {this.state.data.results.map(
+        photo => <Col key={photo.id} span={6} style={{ padding: 10 }}>
+          <a href={photo.links.html} target="_blank">
+            <img className="photo" src={photo.urls.thumb} alt={photo.alt_description}/>
+          </a>
+        </Col>
+      )}
+    </Row>
+  );
   render() {
     return (
       <div className="search-photo">
-        <Input placeholder="Type something to search for image" onChange={this.handleChangeSearchString} />
-        {this.state.loading ? <p>Loading...</p> : <Row className="list-photo">
-          {this.state.data.results.map(
-            photo => <Col key={photo.id} span={6}>
-              <a href={photo.links.html} target="_blank">
-                <img className="photo" src={photo.urls.thumb} />
-              </a>
-            </Col>
-          )}
-        </Row>}
+        <Input placeholder="Type something to search for image" value={this.state.searchString} onChange={this.handleChangeSearchString} />
+        <div  className="list-photo-wrapper">
+        {this.state.loading ? <Spin tip="Loading..." delay={100} >
+          {this.listPhoto()}
+        </Spin> : this.listPhoto()}
+        </div>
         <Pagination simple current={this.state.page} defaultPageSize={16} total={this.state.data.total} onChange={this.handleChangePage} />
       </div>
     );
